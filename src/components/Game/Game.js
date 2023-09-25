afe40b0b960fe29633fb6bbb0f9fbd662f4c6711
Benjamin Bykowski <benjamin.bykowski@gmail.com>
2023-09-25 14:04:31 -0400

    Adds GuesInput and Guess grid

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+
+import GuessInput from '../GuessInput';
+import GuessResults from '../GuessResults';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +13,29 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+
+  const [gameStatus, setGameStatus] = React.useState('running');
+
+const [guesses, setGuesses] = React.useState([]);
+
+function handleSubmitGuess(tentativeGuess) {
+  const nextGuesses = [...guesses, tentativeGuess];
+  setGuesses(nextGuesses);
+
+  if (tentativeGuess.toUpperCase() === answer) {
+    setGameStatus('won');
+  } else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+    setGameStatus('lost');
+  }
+}
+
+  return (<>
+  <GuessResults guesses={guesses} answer={answer} />
+  <GuessInput
+        gameStatus={gameStatus}
+        handleSubmitGuess={handleSubmitGuess}
+      />
+  </>);
 }
 
 export default Game;
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function GuessInput({ gameStatus, handleSubmitGuess }) {
+  const [tentativeGuess, setTentativeGuess] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    handleSubmitGuess(tentativeGuess);
+
+    setTentativeGuess('');
+  }
+
+  return (
+  <form onSubmit={handleSubmit} class="guess-input-wrapper">
+    <label for="guess-input">Enter guess:</label>
+    <input 
+        required
+        disabled={gameStatus !== 'running'}
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        value={tentativeGuess}
+        onChange={(event) => {
+          const nextGuess = event.target.value.toUpperCase();
+          setTentativeGuess(nextGuess);
+        }}
+        id="guess-input"
+        type="text"
+    />
+  </form>);
+}
+
+export default GuessInput;
